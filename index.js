62052e1cd088611d5357811643f7b19487c02e05
Guillaume Rondon <guillaume.rondon@gmail.com>
2020-03-03 10:00:18 +0000

    envoi / reception de messages via les sockets, affichage lors des broadcasts

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const express = require('express');
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-const app = express();
-
-app.get('/', (req, res) => {
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, () => {
-  console.log('server started');
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
